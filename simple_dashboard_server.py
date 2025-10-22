737a311eb53afc0eabe915b832a413bbece70ca6
ishamim <ishamim@purdue.edu>
2025-10-21 22:49:57 -0400

    Update CORS for Amplify

--- simple_dashboard_server.py
+++ simple_dashboard_server.py
@@ -30,7 +30,10 @@ app = FastAPI(title="Marketing Campaign Dashboard", version="1.0.0")
 # Enable CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://main.d296oiluscvwnx.amplifyapp.com/",
+        "http://localhost:5173",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
